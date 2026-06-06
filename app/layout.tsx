bb5f160e39411e9f202f177c507a26443cf9bb06
ruoningz <zrn71717@gmail.com>
2026-06-06 14:03:58 -0700

    add Google Tag Manager (GTM-M6SSBQSW)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins, Libre_Baskerville } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
@@ -35,7 +36,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${poppins.variable} ${libreBaskerville.variable}`}>
+      <head>
+        <Script id="gtm-head" strategy="beforeInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M6SSBQSW');`}</Script>
+      </head>
       <body>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M6SSBQSW" height="0" width="0" style={{ display: "none", visibility: "hidden" }} /></noscript>
         <LenisScroll />
         <ScrollToTop />
         <BackgroundReveal />
